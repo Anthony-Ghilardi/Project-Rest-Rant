e72df93a3ffdc2a91e60982d7347baea67e3f11e
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-05-01 21:06:08 -0700

    Added places controller

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.use('/places', require('./controllers/places'))
+
 app.get('/', (req, res) => {
     res.send('Hello world!')
-})
+});
 
 /********** This 404 route must be below ALL other routes **********/
 app.get('*', (req, res) => {
