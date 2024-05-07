9fe089c6ad0f9260fe2ce7cfcb872f241ae62445
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-05-06 20:33:46 -0700

    wip

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,12 @@ function error404 () {
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops, sorry, we can't find this page!</p>
+                <div>
+                    <img src="/images/manja-vitolic-gKXKBY-C-Dk-unsplash.jpg" alt="Cat" style={{ width: '50%' }}></img>
+                    <div>
+                        Photo by <a href="https://unsplash.com/@madhatterzone">Manja Vitolic</a> on <a href="https://unsplash.com/">Unsplash</a>
+                    </div>
+                </div>
             </main>
         </Def>
     )
