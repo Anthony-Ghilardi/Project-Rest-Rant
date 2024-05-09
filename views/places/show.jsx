168a782ca10d7d60366a636019a8c4985598b3c8
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-05-08 21:48:30 -0700

    WIP

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,14 +1,44 @@
-const React = require('react')
-const Def = require('../default')
+const React = require("react");
+const Def = require("../default");
 
-function show () {
-    return (
-        <Def>
-            <main>
-                <h1>Show Page</h1>
-            </main>
-        </Def>
-    )
+function show(data) {
+  return (
+    <Def>
+      <main className="container">
+        <div>
+          <div className="row">
+            <h1 className="text-primary fs-1">{data.place.name}</h1>
+            <div className="row">
+              <p className="col">{data.place.pic}</p>
+            </div>
+            <div className="col">
+              <h2 className="text-primary fs-1">Rating</h2>
+              <p className="fw-bolder">Not Rated</p>
+            </div>
+            <div className="col">
+              <h3 className="text-primary fs-1">Description</h3>
+              <p className="fw-bolder">
+                Located in {data.place.city}, {data.place.state} and serves{" "}
+                {data.place.cuisines} cuisine
+              </p>
+            </div>
+            <div className="col">
+              <h4 className="text-primary fs-1">Comments</h4>
+              <p className="fw-bolder">No comments yet</p>
+            </div>
+            <a href={`/places/${data.id}/edit_forms`} className="btn btn-warning">
+              Edit
+            </a>
+            <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                <button type="submit" className="btn btn-danger">
+                    Delete
+                </button>
+            </form>
+          </div>
+        </div>
+      </main>
+    </Def>
+  );
 }
 
-module.exports = show
+module.exports = show;
